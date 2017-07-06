9896f91a8887dfcabb23f7327c890eac08d17b8d
Fabian Winnen <fabian@kinemic.de>
2017-07-06 11:17:02 +0200

    Revert "added scripts to build packaged archives"
    This reverts commit 2cc77d4087d90af9e4fe176871dea7bc5c4bed0a.

--- package_android.sh
+++
@@ -1,7 +0,0 @@
-./tensorflow/contrib/makefile/build_all_android.sh
-rm -rf tfcore-android-armeabi-v7a-opt
-cp -r tensorflow/contrib/makefile/gen tfcore-android-armeabi-v7a-opt
-cd tfcore-android-armeabi-v7a-opt
-rm -rf dep bin host_bin host_obj obj protobuf protobuf-host
-cd ..
-zip -r  tfcore-android-armeabi-v7a-opt.zip tfcore-android-armeabi-v7a-opt
--- package_raspberry.sh
+++
@@ -1,7 +0,0 @@
-#./tensorflow/contrib/makefile/build_all_linux.sh
-rm -rf tfcore-raspberry-armhf-opt
-cp -r tensorflow/contrib/makefile/gen tfcore-raspberry-armhf-opt
-cd tfcore-raspberry-armhf-opt
-rm -rf dep bin host_bin host_obj obj protobuf protobuf-host
-cd ..
-zip -r  tfcore-raspberry-armhf-opt.zip tfcore-raspberry-armhf-opt
